f14d5be211efd1b0aa4ea6aa643b47cebc0b71d6
Julian Wilson <WilsonJ22@dyn230160.cc.lehigh.edu>
2023-05-08 18:40:52 -0400

    Updated File

--- schema.ts
+++ schema.ts
@@ -16,6 +16,7 @@ import {
   password,
   timestamp,
   select,
+  integer,
 } from '@keystone-6/core/fields';
 
 // the document field is a more complicated field, so it has it's own package
@@ -148,67 +149,35 @@ export const lists: Lists = {
   }),
 
   //Beginning myschema
-  Plays: list({
+  Metric: list({
     // WARNING
     //   for this starter project, anyone can create, query, update and delete anything
     //   if you want to prevent random people on the internet from accessing your data,
     //   you can find out more at https://keystonejs.com/docs/guides/auth-and-access-control
     access: allowAll,
-
-    // this is the fields for our User list
+  
+    // this is the fields for our Metrics list
     fields: {
-      // by adding isRequired, we enforce that every User should have a name
-      //   if no name is provided, an error will be displayed
-      playid: text({ validation: { isRequired: true } }),
-
-      // we can use this field to see what Posts this User has authored
-      //   more on that in the Post list below
-      //games: relationship({ ref: 'Post.author', many: true }),
-
-      createdAt: timestamp({
-        // this sets the timestamp to Date.now() when the user is first created
-        defaultValue: { kind: 'now' },
-      }),
+      drive: integer({ validation: { isRequired: true } }),
+  
+      play: integer({ validation: { isRequired: true } }),
+  
+      defensiveCall: text({ validation: { isRequired: true } }),
+      
     },
   }),
+  
 
-  Metrics: list({
-    // WARNING
-    //   for this starter project, anyone can create, query, update and delete anything
-    //   if you want to prevent random people on the internet from accessing your data,
-    //   you can find out more at https://keystonejs.com/docs/guides/auth-and-access-control
+  
+
+  Game: list({
+   
     access: allowAll,
 
-    // this is the fields for our User list
-    fields: {
-      // by adding isRequired, we enforce that every User should have a name
-      //   if no name is provided, an error will be displayed
-      value: text({ validation: { isRequired: true } }),
-
-      // we can use this field to see what Posts this User has authored
-      //   more on that in the Post list below
-      type: relationship({ ref: 'MetricType', many: true }),
-
-      createdAt: timestamp({
-        // this sets the timestamp to Date.now() when the user is first created
-        defaultValue: { kind: 'now' },
-      }),
-    },
-  }),
-
-  Games: list({
-    // WARNING
-    //   for this starter project, anyone can create, query, update and delete anything
-    //   if you want to prevent random people on the internet from accessing your data,
-    //   you can find out more at https://keystonejs.com/docs/guides/auth-and-access-control
-    access: allowAll,
-
-    // this is the fields for our Post list
     fields: {
       title: text({ validation: { isRequired: true } }),
 
-      // the document field can be used for making rich editable content
-      //   you can find out more at https://keystonejs.com/docs/guides/document-fields
+
       content: document({
         formatting: true,
         layouts: [
@@ -222,58 +191,21 @@ export const lists: Lists = {
         dividers: true,
       }),
 
-      // with this field, you can set a User as the author for a Post
       author: relationship({
-        // we could have used 'User', but then the relationship would only be 1-way
-        ref: 'User.posts',
+        ref: 'User',
 
-        // this is some customisations for changing how this will look in the AdminUI
-        ui: {
-          displayMode: 'cards',
-          cardFields: ['name', 'email'],
-          inlineEdit: { fields: ['name', 'email'] },
-          linkToItem: true,
-          inlineConnect: true,
-        },
 
-        // a Post can only have one author
-        //   this is the default, but we show it here for verbosity
+  
         many: false,
-      }),
-
-      // with this field, you can add some Tags to Posts
-      tags: relationship({
-        // we could have used 'Tag', but then the relationship would only be 1-way
-        ref: 'Tag.posts',
-
-        // a Post can have many Tags, not just one
-        many: true,
-
-        // this is some customisations for changing how this will look in the AdminUI
-        ui: {
-          displayMode: 'cards',
-          cardFields: ['name'],
-          inlineEdit: { fields: ['name'] },
-          linkToItem: true,
-          inlineConnect: true,
-          inlineCreate: { fields: ['name'] },
-        },
       }),
     },
   }),
 
-  // this last list is our Tag list, it only has a name field for now
   MetricType: list({
-    // WARNING
-    //   for this starter project, anyone can create, query, update and delete anything
-    //   if you want to prevent random people on the internet from accessing your data,
-    //   you can find out more at https://keystonejs.com/docs/guides/auth-and-access-control
+   
     access: allowAll,
 
-    // setting this to isHidden for the user interface prevents this list being visible in the Admin UI
-   
 
-    // this is the fields for our Tag list
     fields: {
       name: text(),
       
