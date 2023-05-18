59e8337a8c0afa3d5a132d9c76834307b7b45ab8
eliataylor <eli@taylormadetraffic.com>
2023-05-18 03:20:58 -0700

    admin

---
+++ admin/config.tsx
@@ -0,0 +1,14 @@
+// admin/config.tsx
+/** @jsxRuntime classic */
+/** @jsx jsx */
+import { jsx } from '@keystone-ui/core';
+
+function CustomLogo () {
+    return <h3 css={{
+        background: 'papayawhip'
+    }}>Custom Logo here</h3>
+}
+
+export const components = {
+    Logo: CustomLogo
+}
